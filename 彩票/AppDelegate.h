384cf1be42c9d7e00fcdcd90172fb6d6fb4e838f
hansonboy <jianwei2013@126.com>
2015-12-01 11:17:03 +0800

    Initial Commit

---
+++ 彩票/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  彩票
+//
+//  Created by wangjianwei on 15/12/1.
+//  Copyright © 2015年 JW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 彩票/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  彩票
+//
+//  Created by wangjianwei on 15/12/1.
+//  Copyright © 2015年 JW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
